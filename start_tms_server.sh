a9381ce1105743e5b35628a19de1c6a4e269067f
CW-B-W <cw.brad.wang@gmail.com>
2025-01-11 22:01:48 +0800

    Use lowercase for database name to prevent potential issues

--- start_tms_server.sh
+++ start_tms_server.sh
@@ -6,12 +6,12 @@ until mysql -w -h mysql -u root -pmaplestory -e "SELECT 1" >/dev/null 2>&1; do
     sleep 3
 done
 
-mysql -w -h mysql -u root -pmaplestory -e "use MapleStory" 1> /dev/null 2>& 1
+mysql -w -h mysql -u root -pmaplestory -e "use maplestory" 1> /dev/null 2>& 1
 if [ "$?" = "1" ]
 then
     echo "Initializing TMS database..."
-    mysql -h mysql -u root -pmaplestory -e "create database MapleStory"
-    mysql -h mysql -u root -pmaplestory MapleStory < /app/init_db/maplestory.sql
+    mysql -h mysql -u root -pmaplestory -e "create database maplestory"
+    mysql -h mysql -u root -pmaplestory maplestory < /app/init_db/maplestory.sql
 fi
 
 echo "Starting TMS server..."
